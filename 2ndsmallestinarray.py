cc6eeb8a5e0195783a988278d53e8cfea00cb8f2
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2023-04-28 21:25:09 +0530

    Add files via upload

---
+++ 2ndsmallestinarray.py
@@ -0,0 +1,15 @@
+import math
+
+a=[10,20,30,40,50,15]
+first=math.inf
+second=math.inf
+
+for i in range(len(a)):
+    if a[i]<first:
+       first=a[i]
+for i in range(len(a)):
+    if a[i]!=first and a[i]<second:
+        second=a[i]
+
+print(second)
+
